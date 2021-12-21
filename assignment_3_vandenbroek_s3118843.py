e9a9484e096af32c4449a88099005fc11ac8ed79
exquize <anna.vandenbroek@hotmail.com>
2021-12-21 13:00:46 +0100

    working namefinder, but slow

--- assignment_3_vandenbroek_s3118843.py
+++ assignment_3_vandenbroek_s3118843.py
@@ -1,4 +1,47 @@
+#!/usr/bin/env python3
 
+#How to run:
+
+###modules###
+import csv
+
+####Main##################################################
+
+#read file line-for line and stores content in empty list readcounts
+readcounts = []
+with open ("white_readcounts.txt", "r") as fh:
+    fh_reader = csv.reader(fh, delimiter ='\t')
+    next(fh_reader)
+    for line in fh_reader:
+        readcounts.append(line)
+
+
+#read file line-for-line and stores content in empty list report
+report = []
+with open ("MGIBatchReport_20211217_155157.txt", "r") as fh:
+    fh_reader = csv.reader(fh, delimiter ='\t')
+    next(fh_reader)
+    for line in fh_reader:
+        report.append(line)
+
+#for rep in report:
+#    print(str(rep))
+
+
+#create empty list reformattedread that will hold the new/cleaned/reformatted data
+
+reformatted_read = []
+
+for gene_data in readcounts:
+    new_data = gene_data    
+    for gene_info in report:        
+        if gene_data[0] == gene_info[0]:            
+            new_data.insert(1, gene_info[3])
+            break
+    reformatted_read.append(new_data)
+print(len(reformatted_read))
+print('test')
+        
 
 
 
