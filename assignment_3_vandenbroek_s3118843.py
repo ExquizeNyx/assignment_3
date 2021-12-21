92113f3438c3e32d402bab749d3694e86dba628b
exquize <anna.vandenbroek@hotmail.com>
2021-12-21 16:15:11 +0100

    finished writing, but lists are slow

--- assignment_3_vandenbroek_s3118843.py
+++ assignment_3_vandenbroek_s3118843.py
@@ -2,51 +2,67 @@
 
 #How to run:
 
-###modules###
-import csv
-
 ####Main##################################################
 
 #read file line-for line and stores content in empty list readcounts
 readcounts = []
 headers = None
 with open ("white_readcounts.txt", "r") as fh:
-    fh_reader = csv.reader(fh, delimiter ='\t')
-    
-    headers = next(fh_reader)
-    for line in fh_reader:
-        readcounts.append(line)
-headers.insert(1,'Name')
+      for line in fh:
+        readcounts.append(line.strip().split('\t'))
 
 #read file line-for-line and stores content in empty list report
-report = []
+report_unfiltered = []
 with open ("MGIBatchReport_20211217_155157.txt", "r") as fh:
-    fh_reader = csv.reader(fh, delimiter ='\t')
-    next(fh_reader)
-    for line in fh_reader:
-        report.append(line)
+    for line in fh:
+        report_unfiltered.append(line.strip().split('\t'))
 
+report = []
+for gene_name in report_unfiltered:
+    if gene_name[2] != "No associated gene":
+        report.append(gene_name)
+    
 #for rep in report:
 #    print(str(rep))
+print(
+    "Gene_ID\t"
+    "Name\t"
+    "SRR1796061\t"
+    "SRR1796064\t"
+    "SRR1796094\t"
+    "SRR1796237\t"
+    "SRR1796312\t"
+    "SRR1796318"
+)
 
-
-#create empty list reformattedread that will hold the new/cleaned/reformatted data
-
+#create empty list reformattedread that will hold the reformatted data
 reformatted_read = []
 
-for gene_data in readcounts:
+for gene_data in readcounts[1:]:
     new_data = gene_data    
     for gene_info in report:        
-        if gene_data[0] == gene_info[0]:            
+        if gene_data[0] == gene_info[0]:
             new_data.insert(1, gene_info[3])
             break
     reformatted_read.append(new_data)
-
-with open('combined_file.csv', 'w', newline='') as outcsv:
-    writer = csv.writer(outcsv)
-    writer.writerow(headers)
-    for output in reformatted_read:
-        writer.writerow(output)
+print(reformatted_read[0])
+with open('combined_file.txt', 'w', newline='') as output:
+    output.write(
+        "Gene_ID\t"
+        "Name\t"
+        "SRR1796061\t"
+        "SRR1796064\t"
+        "SRR1796094\t"
+        "SRR1796237\t"
+        "SRR1796312\t"
+        "SRR1796318\n")
+    for result in reformatted_read:
+        output.write("\t".join(result) + "\n")
+        
+#    writer = csv.DictWriter(outcsv, fieldnames = ["Gene ID", "Name", "SRR1796061", "SRR1796064", "SRR1796094", "SRR1796237", "SRR1796312", "SRR1796318"])
+#    writer.writeheader()
+#    writer.writerows(reformatted_read)
+    
         
 
 
